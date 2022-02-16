cc01fab1a46f915e4678ca93e77c35a2d5407438
Icreative Co <46708376+Icreative-Co@users.noreply.github.com>
2022-02-16 13:21:23 +0300

    Update L O C K E R.bat

--- L O C K E R.bat
+++ L O C K E R.bat
@@ -1,19 +1,56 @@
 @echo off
-
+title DAVE ACE
 color a
 echo ======================
-echo      L O C K E R
+echo   E N I G M A T I C
 echo ======================
+ 
+
 :START
-echo 1.ENCRYPT 
-echo 2.DECRYPT
+echo [1] E N C R Y P T 
+echo [2] D E C R Y P T
+
 set/p "cho=:"
 if %cho%==1 goto LOCK
-if %cho%==1 goto INTRO
+if %cho%==2 goto INTRO
+
+:LOCK
+attrib +s +h Lock
+echo [+]Folder Locked 
+goto End
+
 
 :INTRO
 if EXIST Lock goto UNLOCK
 if NOT EXIST Lock goto MDLOCKER
+
+
+
+:UNLOCK
+set /a tries=3
+cls
+echo ===============
+echo   LOGIN PANEL
+echo ===============
+echo Tries (%tries%)
+echo Enter Password
+set/p "pass=>"
+
+if NOT %pass%==pass goto FAIL
+color a
+attrib -s -h  Lock
+echo [+] Folder Unlocked Successfully
+goto END
+
+
+:FAIL
+cls
+color c
+echo [!]Invalid Password
+pause
+goto UNLOCK
+
+
 
 :CONFIRM
 echo Are You sure you want to lock the folder(Y/N)
@@ -23,31 +60,13 @@ if %cho%==N goto END
 echo invalid choice 
 goto CONFIRM
 
-:LOCK
-ren Lock
-echo Folder locked 
-attrib +s +h Lock
-
-goto End
-
-
-:UNLOCK
-echo Enter password to unlock folder
-
-set/p "pass=>"
-if NOT %pass%==pass goto FAIL
-
-attrib -s -h  Lock
-echo Folder unlocked successfully
-goto End
-
-:FAIL
-echo Invalid Password
-goto UNLOCK
 
 :MDLOCKER 
 md Lock 
 echo Video created successfully
 goto CONFIRM
 
-:End
+:END
+echo [+] Exiting Program.....
+pause
+exit
